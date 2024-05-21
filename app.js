5712ad051fa32c8b4fa12a4cb231b6826641b70c
shanghanrun <idim7@naver.com>
2024-05-21 15:56:09 +0900

    몽고클라우드용주소설정

--- app.js
+++ app.js
@@ -12,8 +12,8 @@ app.use(bodyParser.json())
 app.use('/api', indexRouter) 
 //라우터는 주소가 들어왔을 때, 해야할 행동을 정의한 것이다.
 
-const mongoURI = process.env.MONGODB_LOCAL_URI
-// const mongoURI = process.env.MONGODB_URI    몽고 Atlas cluster용
+// const mongoURI = process.env.MONGODB_LOCAL_URI
+const mongoURI = process.env.MONGODB_URI    //몽고 Atlas cluster용
 // 그리고 package.json에 "start": "node app.js"를 추가해야 된다. 지금은 뺐다.
 console.log('mongodbUri ==>', mongoURI)  // 잘 들어오나 반드시 확인!
 
